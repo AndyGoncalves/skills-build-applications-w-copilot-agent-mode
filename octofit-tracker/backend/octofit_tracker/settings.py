5393eeac3ac49140844140f9b32487d2e742f34d
Andréia Gonçalves <100314549+AndyGoncalves@users.noreply.github.com>
2025-04-08 18:35:06 +0000

    Update Django backend for Codespace URL and test API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pq#zc$z9a0a0zs)88&tjl5k=ql%4v-i%kc*9cq@w11p0e!^6_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'turbo-eureka-wrvwpq66vvc9gg4-8000.app.github.dev']
 
 
 # Application definition
